f2b5f4e743ff61f42ff59ce27a28300f4a9f002f
danielballan <daniel.b.allan@gmail.com>
2015-10-20 19:08:14 -0400

    Include host in display name

--- kernelize/post-link.sh
+++ kernelize/post-link.sh
@@ -10,7 +10,7 @@ echo "Creating kernel for the \"${ENV_NAME}\" env." > $PREFIX/.messages.txt
 mkdir -p $PREFIX/share/jupyter/kernels/env_${ENV_NAME}
 cat > $PREFIX/share/jupyter/kernels/env_${ENV_NAME}/kernel.json <<EOF
 {
- "display_name": "ENV: ${ENV_NAME}",
+ "display_name": "ENV: ${ENV_NAME} on ${HOSTNAME}",
  "language": "python",
  "codemirror_mode": {
   "version": 3,
@@ -18,12 +18,12 @@ cat > $PREFIX/share/jupyter/kernels/env_${ENV_NAME}/kernel.json <<EOF
  },
  "argv": [
   "source",
-  "activate",
+  "${PREFIX}/bin/activate",
   "${ENV_NAME}",
   "&&",
   "${PREFIX}/bin/python",
   "-c",
-  "from IPython.kernel.zmq.kernelapp import main; main()",
+  "\"from IPython.kernel.zmq.kernelapp import main; main()\"",
   "-f",
   "{connection_file}"
  ],
